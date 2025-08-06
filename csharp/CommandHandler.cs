e54087a67d36f14b4dbc91a915203d65a96d1270
AstarLC4036 <1975727002@qq.com>
2025-08-06 22:28:09 +0800

    一个CSharp的命令行版本，但使用78编码的变体

---
+++ csharp/CommandHandler.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace un7cod8
+{
+    /// <summary>
+    /// 一个简单的命令解释器
+    /// </summary>
+    public class CommandHandler
+    {
+        //allow "-pattern <space*n> arg"
+        public readonly static string commandArgMatchPattern = "-[a-zA-z](\\s*)([^\"].{1,}?(\\s|$)|\".{1,}?\")";
+        //not allow: -[a-zA-z]\s([^"]\S{1,}?(\s|$)|"\S{1,}?")
+
+        static Dictionary<string, Action<List<Argument>>> commandDir = new Dictionary<string, Action<List<Argument>>>();
+
+        /// <summary>
+        /// 批量注册所有命令
+        /// </summary>
+        public static void AssignCommands()
+        {
+            AssignCommand("help", (args) => {
+                if (args.Count == 0)
+                {
+                    Console.WriteLine(
+                        "EXIT     退出程序。\n" +
+                        "HELP     获取命令帮助。\n" +
+                        "ENCODE   编码78\n" +
+                        "  -S     源字符串\n" + 
+                        "DECODE   解码78\n" +
+                        "  -S     源字符串");
+                }
+                else
+                {
+                    Console.WriteLine("参数数量错误");
+                }
+            });
+            AssignCommand("exit", (args) => {
+                if (args.Count == 0)
+                {
+                    Console.WriteLine("正在退出程序...");
+                    Program.running = false;
+                }
+                else
+                {
+                    Console.WriteLine("参数数量错误");
+                }
+            });
+            AssignCommand("encode", (args) => {
+                if (args.Count == 1)
+                {
+                    Argument stringArg = args.Find(x => x.pattern == "S");
+                    if(stringArg != null)
+                    {
+                        Console.WriteLine("编码结果: " + Program.Encode(stringArg.value));
+                    }
+                    else
+                    {
+                        Console.WriteLine("参数错误");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("参数数量错误");
+                }
+            });
+            AssignCommand("decode", (args) => {
+                if (args.Count == 1)
+                {
+                    Argument stringArg = args.Find(x => x.pattern == "S");
+                    if (stringArg != null)
+                    {
+                        Console.WriteLine("解码结果: " + Program.Decode(stringArg.value));
+                    }
+                    else
+                    {
+                        Console.WriteLine("参数错误");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("参数数量错误");
+                }
+            });
+        }
+
+        /// <summary>
+        /// 注册单个命令
+        /// </summary>
+        /// <param name="commandName">命令名</param>
+        /// <param name="action">命令行为</param>
+        public static void AssignCommand(string commandName, Action<List<Argument>> action)
+        {
+            commandDir.Add(commandName, action);
+        }
+
+        /// <summary>
+        /// 处理命令
+        /// </summary>
+        /// <param name="command">命令源文本</param>
+        public static void HandleCommand(string command)
+        {
+            string[] commandEle = command.Split(' ');
+
+            //"command -pattern arg" => "command"
+            string commandName;
+            if (commandEle.Length > 0)
+            {
+                commandName = commandEle[0].ToLower();
+                //Console.WriteLine(commandName);
+            }
+            else
+            {
+                Console.WriteLine("输入不能为空");
+                return;
+            }
+
+            List<Argument> args = new List<Argument>();
+
+            //"command -pattern arg -pattern arg" => "-pattern arg ", "-pattern arg "
+            foreach (Match match in Regex.Matches(command, commandArgMatchPattern))
+            {
+                string fullArg = match.Value;
+
+                #region DEBUG
+#if DEBUG
+                Console.WriteLine("fullarg = " + fullArg);
+#endif
+                #endregion
+                //"-pattern arg " => " pattern arg"
+                //" pattern arg " => "pattern", "arg"
+                string[] patternAndArg = fullArg.Split(new char[]{ ' '}, 2);
+                #region DEBUG
+#if DEBUG
+                Console.WriteLine("patternAndArg: " + patternAndArg.Length);
+#endif
+                #endregion
+                string pattern;
+                string arg;
+                if (patternAndArg.Length == 2)
+                {
+                    pattern = patternAndArg[0].Replace("-", "");
+                    arg = patternAndArg[1].Replace("\"", "");
+                    #region DEBUG
+#if DEBUG
+                    Console.WriteLine("pattern = " + pattern);
+                    Console.WriteLine("arg = " + arg);
+#endif
+                    #endregion
+                    args.Add(new Argument(pattern, arg));
+                }
+            }
+
+            Action<List<Argument>> action;
+            if(commandDir.TryGetValue(commandName, out action))
+            {
+                action(args);
+            }
+            else
+            {
+                Console.WriteLine("命令不存在");
+            }
+        }
+
+        /// <summary>
+        /// 命令参数类
+        /// </summary>
+        public class Argument
+        {
+            public string pattern;
+            public string value;
+
+            public Argument()
+            {
+
+            }
+
+            public Argument(string pattern,string value)
+            {
+                this.pattern = pattern;
+                this.value = value;
+            }
+        }
+    }
+}
---
+++ csharp/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Text;
+
+namespace un7cod8
+{
+    public class Program
+    {
+        //运行标识 当为false时退出命令输入循环
+        internal static bool running = true;
+
+        static void Main(string[] args)
+        {
+            //string str1 = Encode("Hello你好");
+            //Console.WriteLine(str1);
+            //string str2 = Decode("8778777 8877878 8878877 8878877 8878888 88777778778888 88888788788778");
+            //Console.WriteLine(str2);
+
+            InitText();
+            CommandHandler.AssignCommands();
+            //命令处理
+            while(running)
+            {
+                Console.Write(" >");
+                string command = Console.ReadLine();
+                CommandHandler.HandleCommand(command);
+            }
+        }
+
+        /// <summary>
+        /// 显示初始化文本
+        /// </summary>
+        static void InitText()
+        {
+            Console.WriteLine("           [UN7COD8 - 文本转换器]");
+            Console.WriteLine("     Source: [Github:YangQi0408] https://github.com/YangQi0408/un7cod8 - AGPL-3.0\n");
+            Console.WriteLine("     Current branch: 'objectivec' by AstarLC");
+            Console.WriteLine("     Enter 'help' for help.\n");
+        }
+
+        /// <summary>
+        /// 编码un7cod8
+        /// </summary>
+        /// <param name="source">源文本</param>
+        /// <returns>编码后的文本</returns>
+        internal static string Encode(string source)
+        {
+            //拆分字符
+            char[] chars = source.ToCharArray();
+            string result = "";
+            //处理字符
+            foreach (char c in chars)
+            {
+                //获取每个字符的unicode对应的byte
+                byte[] bytes = Encoding.Unicode.GetBytes(c.ToString());
+                //固定byte编码
+                string binaryStr1 = Convert.ToString(bytes[0], 2);
+                string encodedStr1 = binaryStr1.Replace("0", "7").Replace("1", "8");
+                string encodedStr2 = "";
+                //非固定byte编码(中文等)
+                if (bytes[1] != 0)
+                {
+                    string binaryStr2 = Convert.ToString(bytes[1], 2);
+                    encodedStr2 = binaryStr2.Replace("0", "7").Replace("1", "8");
+                }
+                //拼合文本
+                result += bytes[1] == 0 ? encodedStr1 + " " : encodedStr2 + "x" + encodedStr1 + " ";
+                //Console.WriteLine(((byte)c).ToString("X4"));
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 解码un7cod8
+        /// </summary>
+        /// <param name="source">源文本</param>
+        /// <returns>解码后的文本</returns>
+        internal static string Decode(string source)
+        {
+            //还原为二进制unicode(byte)
+            string origin = source.Replace("7", "0").Replace("8", "1");
+            //拆分
+            string[] charsInBin = origin.Split(' ');
+            string result = "";
+            //处理解码
+            foreach(string bin in charsInBin)
+            {
+                byte[] bytes = new byte[2];
+
+                //当字符量大于9时视作两个byte代表的字符
+                if (bin.Length >= 9)
+                {
+                    //还原初始byte
+                    string[] substrs = bin.Split('x');
+                    bytes[0] = (byte)Convert.ToInt32(substrs[1], 2);
+                    bytes[1] = (byte)Convert.ToInt32(substrs[0], 2);
+                }
+                else
+                {
+                    //还原初始byte
+                    bytes[0] = (byte)Convert.ToInt32(bin, 2);
+                    bytes[1] = 0;
+                }
+                //unicode解码与拼合
+                result += Encoding.Unicode.GetString(bytes);
+            }
+            return result;
+        }
+    }
+}
